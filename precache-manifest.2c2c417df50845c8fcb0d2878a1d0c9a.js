cdc9bd24452c3ccbe9914a2c1e372a06f9782051
OLIJAC <ojackson@gmail.com>
2018-12-10 14:21:10 -0500

    Updates

---
+++ precache-manifest.2c2c417df50845c8fcb0d2878a1d0c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9837e41aa8bc9296fef8",
+    "url": "/static/js/main.9837e41a.chunk.js"
+  },
+  {
+    "revision": "305ad57b42aa344fab70",
+    "url": "/static/js/1.305ad57b.chunk.js"
+  },
+  {
+    "revision": "9837e41aa8bc9296fef8",
+    "url": "/static/css/main.29cd5810.chunk.css"
+  },
+  {
+    "revision": "603c77360f15dbc0a8b855e13dd26ce3",
+    "url": "/index.html"
+  }
+];
